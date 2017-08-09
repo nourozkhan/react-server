109bac6642d54e2ac8b0a6fadfb14e131dbf9fd5
sabih <siddiquisabih94@gmail.com>
2017-08-09 19:19:14 +0500

    server

--- client/src/components/index.js
+++
@@ -1,2 +0,0 @@
-// export all the components in this file from the same folder it is in.
-export {default as Header} from './App/App.jsx';
--- client/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from 'redux';
-import TodosReducer from './todos_reducer';
-
-const rootReducer = combineReducers({
-    todos: TodosReducer
-});
-export default rootReducer;
